a6d5b5eb5cc1e7397af764e8bcee781a7e940146
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-07-29 13:03:25 -0500

    Initial view completed

--- Auxilio/Start.h
+++ Auxilio/Start.h
@@ -7,8 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface Start : UIViewController
+//Objects
+@property (strong, nonatomic) MPMoviePlayerController *moviePlayer;
+@property (strong, nonatomic) IBOutlet UIView *vVideo;
+@property (strong, nonatomic) IBOutlet UILabel *lblSettings;
+@property (strong, nonatomic) IBOutlet UIButton *btnSettings;
+@property (strong, nonatomic) IBOutlet UILabel *lblStart;
+@property (strong, nonatomic) IBOutlet UIButton *btnStart;
+
+//Actions
+- (IBAction)btnSettingsPressed:(id)sender;
+- (IBAction)btnStartPressed:(id)sender;
 
 
 @end
--- Auxilio/Start.m
+++ Auxilio/Start.m
@@ -13,15 +13,62 @@
 @end
 
 @implementation Start
-
+/**********************************************************************************************/
+#pragma mark - Initialization methods
+/**********************************************************************************************/
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self initController];
 }
-
+//-------------------------------------------------------------------------------
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//-------------------------------------------------------------------------------
+- (void)initController {
+    [self playVideo];
+    
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
+    //[[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    
+    self.lblSettings.layer.borderColor  = [UIColor whiteColor].CGColor;
+    self.lblSettings.layer.borderWidth  = 1;
+    self.lblSettings.clipsToBounds      = YES;
+    self.lblSettings.layer.cornerRadius = 8;
+    
+    self.lblStart.layer.borderColor     = [UIColor whiteColor].CGColor;
+    self.lblStart.layer.borderWidth     = 1;
+    self.lblStart.clipsToBounds         = YES;
+    self.lblStart.layer.cornerRadius    = 8;
+}
+/**********************************************************************************************/
+#pragma mark - Video method
+/**********************************************************************************************/
+-(void)playVideo {
+    //Play video
+    NSString *videoPath = [[NSBundle mainBundle] pathForResource:@"video_children" ofType:@"mp4"];
+    NSURL *videoUrl     = [NSURL fileURLWithPath:videoPath];
+    
+    self.moviePlayer    =[[MPMoviePlayerController alloc] initWithContentURL:videoUrl];
+    [self.moviePlayer.view setFrame:CGRectMake(-100, 0, 1136, 568)];
+    
+    [self.moviePlayer prepareToPlay];
+    [self.moviePlayer setShouldAutoplay:YES];
+    [self.moviePlayer setRepeatMode:MPMovieRepeatModeOne];
+    self.moviePlayer.controlStyle = MPMovieControlStyleNone;
+    [self.vVideo addSubview:self.moviePlayer.view];
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification object:[UIApplication sharedApplication] queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
+        [self.moviePlayer play];
+    }];
+}
+/**********************************************************************************************/
+#pragma mark - Action methods
+/**********************************************************************************************/
 
+- (IBAction)btnSettingsPressed:(id)sender {
+}
+
+- (IBAction)btnStartPressed:(id)sender {
+}
 @end
